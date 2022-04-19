2a30e71421c317e479a687d482077382791d2dce
Vitali Kasaty <vitalik007007@gmail.com>
2022-04-19 22:32:38 +0300

    configuration with annotations(@Component, @Autowired, @Qualifier, @Value)

--- java/com/udemy/spring/spring_introduction/Person.java
+++ java/com/udemy/spring/spring_introduction/Person.java
@@ -1,14 +1,26 @@
 package com.udemy.spring.spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
+	
+	@Autowired
+	@Qualifier("catBean")
 	private Pet pet;
+	@Value("${person.surName}")
 	private String surName;
+	@Value("${person.age}")
 	private int age;
 	
-	public Person(Pet pet) {
-		System.out.println("Person bean is created");
-		this.pet = pet;
-	}
+//	@Autowired
+//	public Person(Pet pet) {
+//		System.out.println("Person bean is created");
+//		this.pet = pet;
+//	}
 	
 	public Person() {
 		System.out.println("Person bean is created");
@@ -18,7 +30,8 @@ public class Person {
 		System.out.println("Hello, my lovely Pet");
 		pet.say();
 	}
-
+	
+//	@Autowired
 	public void setPet(Pet pet) {
 		System.out.println("Class Person: set pet");
 		this.pet = pet;
